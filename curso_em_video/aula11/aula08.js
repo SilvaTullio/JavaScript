f5f37d0f5205f5aba97764f389b114f91b9fe4db
SilvaTullio <tullioaparecidosilvacosta@outlook.com>
2022-12-13 11:32:15 -0300

    revisaro ex11

---
+++ curso_em_video/aula11/aula08.js
@@ -0,0 +1,7 @@
+var vel = 70.6;
+console.log(`A velocidade do seu carro é ${vel} Km/h`);
+if (vel >= 65) {
+  console.log("Voce está multado!");
+} else {
+  console.log("Dirija sempre usando cinto de segurança!");
+}
